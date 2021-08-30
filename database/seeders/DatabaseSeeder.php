4b2bedda7ad81b6eae4399e1186f1435d86f7f1b
enoch454 <enoch454>
2021-08-30 13:36:57 -0500

    Se agregó un registro de usuario en el seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 
 class DatabaseSeeder extends Seeder
@@ -28,5 +29,14 @@ class DatabaseSeeder extends Seeder
                 'pathImagen' => $ite->imagen,
             ]);    
         }
+
+        DB::table('users')->insert([
+            'name' => "admin",
+            'email' => "aegc.45@gmail.com",
+            'email_verified_at' => now(),
+            'password' => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi",
+            'remember_token' => Str::random(10),            
+        ]);
+
     }
 }
