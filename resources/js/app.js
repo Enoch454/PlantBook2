4ba401db28c05b5cc174fe7f16b3b8f3bb43eafc
enoch454 <enoch454>
2021-09-29 03:35:20 -0500

    detalle dashboard-component

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,9 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('home-component', require('./components/HomeComponent.vue').default);
+//Vue.component('dashboard-component', require('./components/DashboardComponent.vue').default);
 Vue.component('dashboard-component', require('./components/DashboardComponent.vue').default);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
